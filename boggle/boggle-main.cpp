e153219af62dbb693bf03bda9e8681504cf32f74
pearcej <jan_pearce@berea.edu>
2019-03-25 08:09:40 -0400

    Adding files to repo

---
+++ boggle/boggle-main.cpp
@@ -0,0 +1,205 @@
+//Modified from https://codereview.stackexchange.com/questions/146877/boggle-board-solver-in-c 
+
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+// Map word lengths to Boggle game points
+unordered_map<int, int> POINTS{ {3, 1}, {5, 2}, {6, 3}, {7, 5}, {8, 11} };
+
+/******************************************************************************/
+
+class Dictionary {
+public:
+	Dictionary();
+	Dictionary(string, unsigned int word_length = 3);
+	~Dictionary();
+
+	unordered_set<string> words;
+	unordered_set<string> prefixes;
+};
+
+Dictionary::Dictionary() {};
+
+// Load the word dictionary and prefixes dictionary from a given file
+Dictionary::Dictionary(string path, unsigned int word_length) {
+	string line;
+	ifstream file(path);
+	if (file.is_open()) {
+		while (getline(file, line)) {
+			if (line.length() >= word_length) {
+				// Add to word dictionary
+				this->words.insert(line);
+
+				// Add to prefixes dictionary
+				for (unsigned int i = 1; i < line.length(); i++) {
+					this->prefixes.insert(line.substr(0, i));
+				}
+			}
+		}
+	}
+	file.close();
+}
+
+Dictionary::~Dictionary() {};
+
+/******************************************************************************/
+
+// Boggle board class
+class Boggle {
+private:
+	struct square {
+		string value;
+		int row;
+		int col;
+		vector<int> neighbors;
+
+		square(int row, int col);
+	};
+
+	const int size;
+	shared_ptr<const Dictionary> dict;
+	unordered_map<int, int> points;
+	vector<square> board;
+	unordered_set<string> found_words;
+	void Words(int position);
+	void Words(int position, string, vector<bool> &visited);
+
+public:
+	Boggle(shared_ptr<const Dictionary> dict, int size = 4, unordered_map<int, int> points = POINTS);
+	~Boggle();
+
+	void BLoad(string letters);
+	void BPrint();
+	int BScore();
+};
+
+// Boggle board constructor
+Boggle::Boggle(shared_ptr<const Dictionary> dict, int size, unordered_map<int, int> points) :
+	dict(dict), size(size), points(points) {
+	int row, col;
+
+	// Add squares to the board
+	for (int i = 0; i < size * size; i++) {
+		row = i / size;
+		col = i % size;
+		board.push_back(square(row, col));
+	}
+
+	// Add each square's neighbors
+	vector<int> shift{ -1, 0, 1 };
+	for (square &sq : board) {
+		for (int row_shift : shift) {
+			for (int col_shift : shift) {
+				row = sq.row + row_shift;
+				col = sq.col + col_shift;
+				if (row >= 0 && row < size && col >= 0 && col < size && !(row_shift == 0 && col_shift == 0)) {
+					sq.neighbors.push_back(row * size + col);
+				}
+			}
+		}
+	}
+}
+
+Boggle::~Boggle() {}
+
+// Boggle square constructor
+Boggle::square::square(int row, int col) {
+	this->row = row;
+	this->col = col;
+}
+
+// Load a string of letters into the board
+void Boggle::BLoad(string letters) {
+	int i = 0;
+	for (square &it : board) {
+		it.value = letters[i];
+		i += 1;
+	}
+	// Clear any previously found words
+	found_words.clear();
+}
+
+// Print the board
+void Boggle::BPrint() {
+	for (const square &sq : board) {
+		cout << sq.value << " ";
+		if (sq.col == size - 1) {
+			cout << endl;
+		}
+	}
+}
+
+// Find all words, then calculate the score
+int Boggle::BScore() {
+	int score = 0;
+
+	// Find words for all squares on the board
+	for (unsigned int i = 0; i < board.size(); i++) {
+		Words(i);
+	}
+
+	// For each word, look up points and add to the score
+	unordered_map<int, int>::iterator point;
+	for (const string &word : found_words) {
+		// Find the smallest point map key greater than word length, then move back one step
+		// to get the largest key less than or equal to word length, e.g. 4->5->3
+		point = points.upper_bound(word.length());
+		--point;
+		score += point->second;
+	}
+
+	return score;
+}
+
+// Top-level Words function overload; sets up visited vector, whose size is determined at runtime
+void Boggle::Words(int position) {
+	vector<bool> visited(size*size, false);
+	Words(position, "", visited);
+}
+
+// Find all words starting at a given position
+void Boggle::Words(int position, string string, vector<bool> &visited) {
+
+	square &sq = board[position];
+	string = string + sq.value;
+	visited[position] = true;
+
+	// If the string is a word, add it to the found words
+	if (dict->words.find(string) != dict->words.end()) {
+		found_words.insert(string);
+	}
+
+	// If the string is a prefix, continue looking
+	if (dict->prefixes.find(string) != dict->prefixes.end()) {
+		for (const int &neighbor : sq.neighbors) {
+			if (!visited[neighbor]) {
+				Words(neighbor, string, visited);
+			}
+		}
+	}
+
+	visited[position] = false;
+}
+
+/******************************************************************************/
+
+int main() {
+	char hold;
+
+	cout << "working... please be patient..." << endl;
+
+	auto dict = make_shared<const Dictionary>("mydict.txt");
+	Boggle b(dict);
+	b.BLoad("serspatglinesers");
+	b.BPrint();
+	cout << "Your score is: " << b.BScore() << endl;
+
+	cin >> hold;
+}
